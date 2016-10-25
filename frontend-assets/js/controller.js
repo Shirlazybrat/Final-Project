c0a12ca965945c6eecabd972135d71c16e0be301
Shirlette Chambers <shirlette.chambers@gmail.com>
2016-10-25 12:52:33 -0400

    Created different views and embedded calendar

--- frontend-assets/js/controller.js
+++ frontend-assets/js/controller.js
@@ -1,5 +1,36 @@
-var finalProject = angular.module('finalProject', []);
+var finalProject = angular.module('finalProject', ['ngRoute']);
+
+finalProject.config(function($routeProvider){
+	$routeProvider
+
+	//routes for pages
+	.when('/',{
+		templateUrl: 'frontend-assets/views/home.html',
+		controller: 'mainController'
+	})
+	.when('/login',{
+		templateUrl: 'frontend-assets/views/login.html',
+		controller: 'mainController'
+	})
+	.when('/register',{
+		templateUrl: 'frontend-assets/views/register.html',
+		controller: 'mainController'
+	})
+	.when('/portal',{
+		templateUrl: 'frontend-assets/views/portal.html',
+		controller: 'mainController'
+	})
+	.when('/boards',{
+		templateUrl: 'frontend-assets/views/boards.html',
+		controller: 'mainController'
+	})
+	.otherwise({
+		redirectTo: '/'
+	})
+});
+
+
 
 finalProject.controller('mainController', function($scope){
-	$scope.message = 'Test' 
+	$scope.home = 'home';
 });
