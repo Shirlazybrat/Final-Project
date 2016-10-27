5a1ad96bd7481eeb603ce03de5c3621f8f3e535d
Shirlette Chambers <shirlette.chambers@gmail.com>
2016-10-27 17:41:36 -0400

    installed mongodb and mongoose

---
+++ backend/models/accounts.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var Account = new Schema ({
+	username: {type: String, required: true},
+	password: {type: String, required: true},
+	email: {type: String, required: true},
+	token: String,
+	name: String,
+	wed_date: String,
+	location: String,
+	budget: Number,
+	orders: {
+		item: String,
+		amount: Number,
+		total: Number,
+		date: Number
+		}
+	});
+
+module.exports = mongoose.model('Account', Account);
+
+ // Use node mailer to outomate the orders to her email- https://nodemailer.com/
